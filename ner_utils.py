45835e26932fcf4f8dad718f75d294afd7d6bce3
Vidyalakshmi <115562328+Vidyalakshmi04@users.noreply.github.com>
2026-02-06 23:01:45 +0530

    Update ner_utils.py

--- ner_utils.py
+++ ner_utils.py
@@ -2,10 +2,8 @@ import spacy
 
 try:
     nlp = spacy.load("en_core_web_sm")
-except OSError:
-    from spacy.cli import download
-    download("en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
+except:
+    nlp = spacy.blank("en")
 
 def extract_entities(text):
     doc = nlp(text)
